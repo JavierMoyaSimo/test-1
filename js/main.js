0c241f916f5998f1ea0c30ba04c4ccaa03da989f
Javi <javiermoyasimo@gmail.com>
2023-06-07 12:24:28 +0200

    feat: styles added. Main styles and mobile styles.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let date = new Date();
 
 let currentDate =
   date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-document.getElementById("currentDate").innerHTML = currentDate;
+document.getElementById("currentDate").innerHTML = `[${currentDate}]`;
 
-console.log(date);
-console.log(currentDate);
+
